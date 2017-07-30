c377bd9205a67e7846f85a80e8ae6f4a2970e5c4
Belchenkov <u608110@gmail.com>
2017-07-30 19:45:57 +0300

    Add & Delete Notes

--- client/main.js
+++ client/main.js
@@ -8,3 +8,35 @@ Template.body.helpers({
         return Notes.find({});
     }
 });
+
+Template.add.events({
+    'submit .add-form': function() {
+        event.preventDefault();
+
+        // Get input value
+        const target = event.target;
+        const text = target.text.value;
+
+        //console.log(text);
+
+        // Insert Note into collection
+        Notes.insert({
+            text,
+            createdAt: new Date()
+        });
+
+        target.text.value = '';
+        $('#addModal').modal('close');
+
+
+        return false;
+    }
+});
+
+Template.note.events({
+    'click .delete-note': function() {
+        Notes.remove(this._id);
+
+        return false;
+    }
+});
